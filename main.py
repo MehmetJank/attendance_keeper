34d0a12dd7d280689542a2e4277d46c82bfa9939
MehmetJank <92443831+MehmetJank@users.noreply.github.com>
2023-04-21 17:33:00 +0300

    add list for listbox and combobox

--- main.py
+++ main.py
@@ -1,8 +1,57 @@
 from tkinter import Tk, Frame, Entry, Button, END, RIGHT, Label, Listbox
 from tkinter import ttk
 
+from openpyxl import load_workbook
 
 class AttendanceKeeperApp(Frame):
+
+    def import_list(self):
+        # Excel dosyasını açar
+        wb = load_workbook('ENGR 102 Student List.xlsx')
+
+        # Aktif sayfayı seçer
+        ws = wb.active
+
+        # ----------------- Bölüm Seçimi -----------------
+        # combo_box_items listesini tanımlar. Yani bolumlerin listesi
+        combo_box_items = []
+
+        # Ilk satırı haric D sütunundaki tüm hücreleri ekler
+        for cell in ws['D'][1:]:
+            combo_box_items.append(cell.value)
+
+        # combo_box_items listesindeki tekrar eden elemanları siler
+        combo_box_items = list(set(combo_box_items))
+
+        # combo_box_items listesindeki elemanları alfabetik sıraya koyar
+        combo_box_items.sort()
+
+        # combo_box_items listesindeki elemanları ekrana yazdırır
+        # for items in combo_box_items:
+            # print(items)
+
+        # user_choice_section = int(input("Bolum Seciniz:"))
+        # user_choice_section = combo_box_items[user_choice_section - 1]
+        # print(user_choice_section)
+
+
+        # min row 2. satirdan baslar cunku basliklar olmayacak, min col 1. sutundan baslayip, max col 4. sutuna kadar döngüye alir
+
+        # ----------------- Öğrenci Seçimi Function -----------------
+
+
+        def section_students(section: str):
+            # min row 2. satirdan baslar cunku basliklar olmayacak, min col 1. sutundan baslayip, max col 4. sutuna kadar döngüye alir
+            for row in ws.iter_rows(min_row=2, min_col=1, max_col=4):
+                cell = row[3]  # 4. sutun
+                if cell.value == section:  # 4. sutunun degeri bolumun degerine esitse
+                    # 2. sutunun degerini bosluklara gore ayirir
+                    name_surname = row[1].value.split()
+                    # 2. sutunun ilk elemani soyadi, ikinci elemani adi, 1. sutunun degeri numarasi
+                    print(name_surname[1], name_surname[0], row[0].value)
+
+        return combo_box_items
+
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.parent = parent
@@ -52,7 +101,6 @@ class AttendanceKeeperApp(Frame):
             text=text,
             width=width,
             height=height,
-            background="red",
         )
         label.grid(
             row=row,
@@ -64,7 +112,7 @@ class AttendanceKeeperApp(Frame):
             sticky=sticky,
         )
 
-    def custom_listbox(self, row: int, column: int):
+    def custom_listbox(self, row: int, column: int, list_values : list = []):
         listBox = Listbox(width=30, height=5)
         listBox.grid(
             row=row,
@@ -75,8 +123,10 @@ class AttendanceKeeperApp(Frame):
             pady=3,
             sticky="nswe",
         )
+        for item in list_values:
+            listBox.insert(END, item)
 
-    def custom_combo_box(self, row: int, column: int, width: int, height: int):
+    def custom_combo_box(self, row: int, column: int, width: int, height: int, list_values: list = [0]):
         combo = ttk.Combobox(width=width, height=height)
         combo.grid(
             row=row,
@@ -87,6 +137,8 @@ class AttendanceKeeperApp(Frame):
             pady=3,
             sticky="nswe",
         )
+        combo["values"] = list_values
+        combo.current(0)
 
     def initUI(self):
         # Boyutlandırma
@@ -164,9 +216,10 @@ class AttendanceKeeperApp(Frame):
         )
 
         # Row 3
-        self.custom_listbox(row=3, column=0)
+        my_test_list = ["test1", "test2", "test3", "test4", "test5"]
+        self.custom_listbox(row=3, column=0, list_values=my_test_list)
 
-        self.custom_combo_box(row=3, column=2, width=15, height=5)
+        self.custom_combo_box(row=3, column=2, width=15, height=5, list_values=self.import_list())
 
         self.custom_listbox(row=3, column=3)
 
